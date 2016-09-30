a4142984a29175ffd5656b63b13713c336b1809b
Diana O'Haver <diohaver@Dianas-MacBook-Pro.local>
2016-09-30 16:55:07 -0700

    Allowing new db fields through as params

--- prog2/recipebook/app/controllers/recipes_controller.rb
+++ prog2/recipebook/app/controllers/recipes_controller.rb
@@ -69,6 +69,6 @@ class RecipesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def recipe_params
-      params.require(:recipe).permit(:name, :description, :instructions, :servings, :cook_time, :publish?)
+      params.require(:recipe).permit(:name, :description, :instructions, :servings, :cook_time, :published)
     end
 end
--- prog2/recipebook/app/controllers/users_controller.rb
+++ prog2/recipebook/app/controllers/users_controller.rb
@@ -73,6 +73,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :username, :password)
+      params.require(:user).permit(:name, :username, :password, :public_profile)
     end
 end
