5ae38c30d37f412c511ff0baca4da255d2ce5f6d
Diana O'Haver <diohaver@Dianas-MacBook-Pro.local>
2016-10-20 15:08:22 -0700

    Remove weird .DS_Store files hopefully

--- prog3/recipebook/config/routes.rb
+++ prog3/recipebook/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'users/:id/recipes' => 'users#recipes', as: 'user_recipes'
+  get 'recipes/:id/ingredients' => 'recipes#list_ingredients', as: 'list_ingredients'
+  post 'recipes/:id/ingredients' => 'recipes#add_ingredients', as: 'add_ingredients'
+  put 'recipes/:id/ingredients' => 'recipes#add_ingredients', as: 'update_ingredients'
 
   resources :recipes
   resources :users
