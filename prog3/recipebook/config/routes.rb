56dbc633e629a33975b21a513fc29011e40e043a
Diana O'Haver <diohaver@Dianas-MacBook-Pro.local>
2016-10-22 15:55:53 -0700

    Trying to fix deploy

--- prog3/recipebook/config/routes.rb
+++ prog3/recipebook/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   get 'users/:id/recipes' => 'users#recipes', as: 'user_recipes'
   get 'recipes/:id/ingredients' => 'recipes#list_ingredients', as: 'list_ingredients'
   post 'recipes/:id/ingredients' => 'recipes#add_ingredients', as: 'add_ingredients'
-  put 'recipes/:id/ingredients/:recipe_id' => 'recipes#update_ingredients', as: 'update_ingredients'
+  put 'recipes/:id/ingredients/:ingredient_id' => 'recipes#update_ingredients', as: 'update_ingredients'
   get 'recipes/ingredients' => 'recipes#all_ingredients', as: 'all_ingredients'
-  delete 'recipes/:id/ingredients/:recipe_id' => 'recipes#delete_ingredient', as: 'delete_ingredient'
+  delete 'recipes/:id/ingredients/:ingredient_id' => 'recipes#delete_ingredient', as: 'delete_ingredient'
   delete 'recipes/:id/ingredients' => 'recipes#delete_all_ingredients', as: 'delete_all'
 
   resources :recipes
