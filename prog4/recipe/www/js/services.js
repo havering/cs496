8d490e4ad6534410b4a23ecfa0e4631f21535ef1
Diana O'Haver <diohaver@Dianas-MacBook-Pro.local>
2016-10-26 16:02:36 -0700

    First pass at Ionic app - interacting via GET with my API

---
+++ prog4/recipe/www/js/services.js
@@ -0,0 +1,5 @@
+angular.module('starter.services', ['ngResource'])
+
+.factory('Recipe', function ($resource) {
+    return $resource('http://recipe.ezmaz2hnxw.us-west-2.elasticbeanstalk.com/recipes/:recipe_id');
+});
